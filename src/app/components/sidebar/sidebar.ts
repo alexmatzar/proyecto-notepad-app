70eb2160e81c23536f10decad2245d9701819cbe
sergiogiomez17 <sgomeztoma@gmail.com>
2026-05-25 09:54:03 -0600

    Sidebar dropdown y more panel agregado

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,15 +1,20 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
+  standalone: true,
   imports: [],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.css'
+  styleUrls: ['./sidebar.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SidebarComponent {
   @Input() activeView: string = 'notes';
   @Input() isPinned: boolean = false;
   @Output() viewChange = new EventEmitter<string>();
+  showMore= false;
 
   isHovered = false;
 
